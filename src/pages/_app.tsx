3be63a87f05c5482cb72d32f57812c3bd6f756b4
Wildan Najah Wildiansyah <wildan.najah@gmail.com>
2023-03-29 17:39:46 +0700

    add ssg to home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import '@/styles/globals.css';
 import getQueryClient from '@/utils/getQueryClient';
 import type {AppProps} from 'next/app';
 import {useState} from 'react';
-import {QueryClientProvider} from 'react-query';
+import {Hydrate, QueryClientProvider} from 'react-query';
 
-export default function App({Component, pageProps}: AppProps) {
+type Props = AppProps<{
+	dehydratedState: unknown;
+}>;
+
+export default function App({Component, pageProps}: Props) {
 	const [queryClient] = useState(getQueryClient);
 	return (
 		<QueryClientProvider client={queryClient}>
-			<Component {...pageProps} />
+			<Hydrate state={pageProps.dehydratedState}>
+				<Component {...pageProps} />
+			</Hydrate>
 		</QueryClientProvider>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,28 @@
-import {useInfQueryPokemons} from '@/queries/pokemons';
+import {fetchPokemons, useInfQueryPokemons} from '@/queries/pokemons';
+import getQueryClient from '@/utils/getQueryClient';
+import {type GetStaticPropsResult} from 'next';
+import {dehydrate, type DehydratedState} from 'react-query';
+
+const initialState = {name: '', typeId: 0};
+
+type Result = GetStaticPropsResult<{dehydratedState: DehydratedState}>;
+
+export async function getStaticProps(): Promise<Result> {
+	const queryClient = getQueryClient();
+	await queryClient.fetchInfiniteQuery(['pokemons', initialState], fetchPokemons);
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const dehydratedState = JSON.parse(JSON.stringify(dehydrate(queryClient)));
+
+	return {
+		props: {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+			dehydratedState,
+		},
+	};
+}
 
 export default function Home() {
-	const filter = {name: '', typeId: 0};
-	const {data} = useInfQueryPokemons(filter);
+	const {data} = useInfQueryPokemons(initialState);
 	const {pages} = data ?? {};
 	const pokemonSpecies = pages!.flat();
 	return (
