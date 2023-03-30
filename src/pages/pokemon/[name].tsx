8881be687716d095c9949433f3d13a92414f8ef6
Wildan Najah Wildiansyah <wildan.najah@gmail.com>
2023-03-30 17:22:18 +0700

    add background color to dynamic route

--- src/pages/pokemon/[name].tsx
+++ src/pages/pokemon/[name].tsx
@@ -6,6 +6,7 @@ import {
 	type GetStaticPropsResult,
 } from 'next';
 import {useRouter} from 'next/router';
+import {useEffect} from 'react';
 import {dehydrate, type DehydratedState} from 'react-query';
 
 type Context = GetStaticPropsContext<{name: string}>;
@@ -48,5 +49,15 @@ export default function PokemonDetail() {
 	const pokemonName = Array.isArray(name) ? name[0].toString() : name.toString();
 	const {data} = useQueryPokemon(pokemonName);
 	const pokemon = data!;
-	return <div>{pokemon.name}</div>;
+	const types = pokemon.pokemon_v2_pokemontypes.map(item => item.pokemon_v2_type.name);
+	useEffect(() => {
+		document.body.classList.add(`bg-elm-${types[0]}`);
+
+		// Clean up the effect when the component unmounts
+		return () => {
+			document.body.classList.remove(`bg-elm-${types[0]}`);
+		};
+	}, [types]);
+
+	return <div className={`bg-elm-${types[0]} h-full`}>{pokemon.name}</div>;
 }
